6cf447dc0eaea509d0ce4b18ebe3d33f6916d143
Yousif <yousuf1212121@gmail.com>
2022-11-04 17:31:41 +0300

    done

--- src/seeders/index.js
+++ src/seeders/index.js
@@ -1,21 +1,26 @@
-const mongoose = require('mongoose')
-require('dotenv').config()
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const LOCALHOST = process.env.HOST;
 const DB_PORT = process.env.PORT;
 const DATABASE = process.env.DATABASE;
 
-const seed = require('./rolesSeeder')
-mongoose.connect(`mongodb://${LOCALHOST}:${DB_PORT}/${DATABASE}`);
-const db = mongoose.connection;
-db.on('error',(e)=>{
-    console.error(e)
-})
+const seed = require("./rolesSeeder");
 
-db.once('open', async ()=>{
-    console.log('Connected to DB successfully')
-    console.log('Seeding Started')
-    await seed()
-    console.log('Data seeded')
-    db.close()
-})
+if (process.env.ENVIRONMENT == "development") {
+  mongoose.connect(`mongodb://${LOCALHOST}:${DB_PORT}/${DATABASE}`);
+} else {
+  mongoose.connect(process.env.MONGODB_CLOUD_CON);
+}
+const db = mongoose.connection;
+db.on("error", (e) => {
+  console.error(e);
+});
+
+db.once("open", async () => {
+  console.log("Connected to DB successfully");
+  console.log("Seeding Started");
+  await seed();
+  console.log("Data seeded");
+  db.close();
+});
